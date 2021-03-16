bd67aa5c4ba6378715147036447149807fff3372
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-03-16 17:53:51 -0400

    add gauge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import socketIOClient from "socket.io-client";
+import socketIOClient from 'socket.io-client';
 import './App.css';
+import GaugeChart from 'react-gauge-chart';
 
 const WS_ENDPOINT = "ws://127.0.0.1:5000";
 const socket = socketIOClient(WS_ENDPOINT);
@@ -19,6 +20,10 @@ function App() {
       <header className="App-header">
         <h1>Data from server:</h1>
         <div><pre>{response ? JSON.stringify(response, 2) : 'Waiting for initial data...'}</pre></div>
+        <GaugeChart
+          percent={(response.counter / 100.0)}
+          animate={false}
+        />
       </header>
     </div>
   );
