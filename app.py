6891a906c9bbb9d795049121bd74541e43e5dd65
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-03-16 16:13:48 -0400

    counter demo

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ import os
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
-FRONTEND_ORIGIN = "http://127.0.0.1:3000"
+FRONTEND_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins=[FRONTEND_ORIGIN])
+socketio = SocketIO(app, cors_allowed_origins=FRONTEND_ORIGINS)
+
+test_counter = 1
 
 @app.route('/')
 def index():
     return "Connect with Socket.IO!"
 
-@socketio.on('connect')
+@socketio.on('DataRequest')
 def test_connect():
-    emit('NewData', {'data':'hello, world'})
+    global test_counter
+    emit('NewData', {'data':'hello, world', 'counter': test_counter})
+    test_counter += 1
 
 if __name__ == '__main__':
     socketio.run(app)
