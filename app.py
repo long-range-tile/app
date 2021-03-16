b68acf88ef49178578938b8774624379929d03c2
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-03-16 17:49:26 -0400

    move logic into pi.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
 import eventlet
 
+import pi
+
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
 FRONTEND_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
-UPDATE_INTERVAL_SEC = 1
+UPDATE_INTERVAL_SEC = 0.08
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins=FRONTEND_ORIGINS)
-
-test_counter = 1
 
 @app.route('/')
 def index():
@@ -18,9 +18,7 @@ def index():
 
 @socketio.on('connect')
 def emit_current_data():
-    global test_counter
-    socketio.emit('NewData', {'data':'hello, world', 'counter': test_counter})
-    test_counter += 1
+    socketio.emit('NewData', pi.get_data())
 
 def run_timer():
     while True:
---
+++ pi.py
@@ -0,0 +1,11 @@
+import itertools
+
+up_range = range(0, 101)
+down_range = range(99, 0, -1)
+one_cycle = list(up_range) + list(down_range)
+cycled_iterator = itertools.cycle(one_cycle)
+
+def get_data():
+    """ Returns a dictionary to be converted to JSON for the frontend """
+    global cycled_iterator
+    return {'data':'hello, world', 'counter': next(cycled_iterator)}
