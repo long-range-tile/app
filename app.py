770d3d88f027234f9fddc0d56de786b21fbed825
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-03-16 15:55:53 -0400

    send test data over socket

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ import os
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
+FRONTEND_ORIGIN = "http://127.0.0.1:3000"
+
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins=[FRONTEND_ORIGIN])
 
 @app.route('/')
 def index():
-    return 'Hello, world'
+    return "Connect with Socket.IO!"
 
 @socketio.on('connect')
 def test_connect():
-    emit('after connect',  {'data':'Lets dance'})
+    emit('NewData', {'data':'hello, world'})
 
 if __name__ == '__main__':
     socketio.run(app)
