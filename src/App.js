3645983aa5d0cb7bac1b835a805de0cee4c04747
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-03-16 17:58:39 -0400

    fix json pretty-printing

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Data from server:</h1>
-        <div><pre>{response ? JSON.stringify(response, 2) : 'Waiting for initial data...'}</pre></div>
+        <div align="left"><pre>{response ? JSON.stringify(response, null, 2) : 'Waiting for initial data...'}</pre></div>
         {response && <GaugeChart
           percent={(response.counter / 100.0)}
           animate={false}
