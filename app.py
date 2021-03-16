50eb6b156d1a8cdb27220f0ff675786880873818
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-03-16 16:56:09 -0400

    timer on backend

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+import eventlet
 
 from flask import Flask
 from flask_socketio import SocketIO, emit
 
 FRONTEND_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
+UPDATE_INTERVAL_SEC = 1
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins=FRONTEND_ORIGINS)
@@ -14,11 +16,17 @@ test_counter = 1
 def index():
     return "Connect with Socket.IO!"
 
-@socketio.on('DataRequest')
-def test_connect():
+@socketio.on('connect')
+def emit_current_data():
     global test_counter
-    emit('NewData', {'data':'hello, world', 'counter': test_counter})
+    socketio.emit('NewData', {'data':'hello, world', 'counter': test_counter})
     test_counter += 1
 
+def run_timer():
+    while True:
+        eventlet.spawn(emit_current_data).wait()
+        eventlet.sleep(UPDATE_INTERVAL_SEC)
+
 if __name__ == '__main__':
-    socketio.run(app)
+    eventlet.spawn(run_timer)
+    eventlet.spawn(socketio.run, app).wait()
