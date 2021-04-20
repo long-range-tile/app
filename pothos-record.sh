74f246378728fa25f8469f465e1fcee85fee583e
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2021-04-20 18:20:57 -0400

    mkfifo before running Pothos

--- pothos-record.sh
+++ pothos-record.sh
@@ -4,4 +4,6 @@ if [ ! -d "$HOME/sdr-ble-demo" ]; then
   git clone https://github.com/DesignSparkrs/sdr-ble-demo/ ~/sdr-ble-demo
 fi
 
+mkfifo /tmp/fifo1
+
 PothosFlow ~//btle_printer_float32.pth 2>&1 | grep "Address" | cut -f4- -d' ' | sed 's!\\!\\\\\\\\!g' | sed 's/\x1b\[[0-9;]*m//g' > /tmp/fifo1
